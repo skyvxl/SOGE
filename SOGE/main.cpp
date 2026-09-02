db8de9de58d0b25ca5ed907c5f06f5ec307d941f
skyvxl <wivvz@yandex.ru>
2026-09-02 19:54:25 +0300

    rename from vulkan to opengl

--- SOGE/main.cpp
+++ SOGE/main.cpp
@@ -30,7 +30,7 @@ int main() {
 
   glfwMakeContextCurrent(window);
 
-  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cerr << "Failed to initialize GLAD\n";
     glfwDestroyWindow(window);
     glfwTerminate();
