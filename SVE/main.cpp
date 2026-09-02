e85877b1b6952a6f35bd9779746886bfa06e87ad
skyvxl <wivvz@yandex.ru>
2026-09-02 19:37:05 +0300

    Добавьте файлы проекта.

---
+++ SVE/main.cpp
@@ -0,0 +1,55 @@
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format on
+
+#include <iostream>
+
+static void framebufferSizeCallback(GLFWwindow*, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window =
+      glfwCreateWindow(1280, 720, "LearnOpenGL", nullptr, nullptr);
+
+  if (window == nullptr) {
+    std::cerr << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+    std::cerr << "Failed to initialize GLAD\n";
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+  glViewport(0, 0, 1280, 720);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
